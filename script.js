a964ac2818b908ac1ffe742e2ffa97e75950da10
Summer Williams <summer@Summers-Air.attlocal.net>
2023-07-05 09:41:55 -0500

    set up prompts to ask what values is wanted in password

--- script.js
+++ script.js
@@ -4,12 +4,26 @@ var number = "0123456789".split("")
 var special = "!@#$%^&*()_+".split("")
 
 function generatePassword() {
+  var beforeReady = []
+
   var passwordLength = prompt("How long would you like your password to be? (8-128)")
   if(passwordLength < 8 || passwordLength > 128) {
     alert("Your password must be between 8-128 charcters. Please restart")
     generatePassword()
   } else {
-      
+    if(confirm("Do you want uppercase in your password?")) {
+      beforeReady = beforeReady.concat(uppercase)
+    }
+    if(confirm("Do you want lowercase in your password?")) {
+      beforeReady = beforeReady.concat(lowercase)
+    }
+    if(confirm("Do you want numebrs in your password?")) {
+      beforeReady = beforeReady.concat(number)
+    }
+    if(confirm("Do you want special characters in your password?")) {
+      beforeReady = beforeReady.concat(special)
+    }
+    console.log(beforeReady)
   }
 }
 
