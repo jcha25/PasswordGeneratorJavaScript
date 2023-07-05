68ce78452778670fc04a93695e2dfc79cf1cc844
Summer Williams <summer@Summers-Air.attlocal.net>
2023-07-05 09:38:23 -0500

    set up passwordLength prompt

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ var lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
 var number = "0123456789".split("")
 var special = "!@#$%^&*()_+".split("")
 
+function generatePassword() {
+  var passwordLength = prompt("How long would you like your password to be? (8-128)")
+  if(passwordLength < 8 || passwordLength > 128) {
+    alert("Your password must be between 8-128 charcters. Please restart")
+    generatePassword()
+  } else {
+      
+  }
+}
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
